9a186f6d526e4baf71e9ac155e93e5f4cee285e1
praveensri453 <praveensri92@gmail.com>
2024-12-03 11:20:38 +0530

    expense-infra-dev

---
+++ 70-acm/variables.tf
@@ -0,0 +1,25 @@
+
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+
+variable "zone_name" {
+    default = "aathidya.online"
+}
+
+variable "zone_id" {
+    default = "Z09912121MS725XSKH1TG"
+}
